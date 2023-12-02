798de5f54d85c3f43e86f92e1f9dd6e8faeaf770
ayberktmn <ayberktemin@gmail.com>
2023-12-02 13:20:25 +0300

    homefragment uı done

--- app/src/main/java/com/ayberk/earthquakeapp/HomeFragment.kt
+++ app/src/main/java/com/ayberk/earthquakeapp/HomeFragment.kt
@@ -6,19 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.addCallback
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.ayberk.earthquakeapp.adapter.EarthquakeAdapter
 import com.ayberk.earthquakeapp.databinding.FragmentHomeBinding
+import com.ayberk.earthquakeapp.viewmodel.EarthquakeViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 
+@AndroidEntryPoint
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
     private var isBackPressed = false
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-
-        }
-    }
+    private lateinit var adapter: EarthquakeAdapter
+    private val viewModel : EarthquakeViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,6 +35,34 @@ class HomeFragment : Fragment() {
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner){
             isBackPressed = true
         }
+
+        initRecycler()
+        fecthEarthquakes()
+
+        viewModel.getEarthquake().observe(viewLifecycleOwner, Observer { t ->
+            t?.let {
+                adapter.setEarthquakeList(t.result)
+            }
+        })
         return view
+    }
+
+    fun initRecycler(){
+        adapter = EarthquakeAdapter()
+        binding.rcyclerEarthquakes.adapter = adapter
+
+        binding.rcyclerEarthquakes.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+
+    }
+
+    fun fecthEarthquakes(){
+        CoroutineScope(Dispatchers.Main).async {
+            viewModel.LoadEarthquake()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
